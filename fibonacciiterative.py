255b5d8c69f8514f75c702c536542278ac2e2ad7
PD <dave.sfsu@gmail.com>
2017-12-13 23:17:32 -0800

    Memoized solution

---
+++ fibonacciiterative.py
@@ -0,0 +1,21 @@
+from datetime import datetime, timedelta
+
+
+def fib (n, memo):
+    if memo[n] is not None:
+        return memo[n]
+    if n == 1 or n == 2:
+        memo[n] = 1
+        result = 1
+    else:
+        result = fib(n-1, memo) + fib(n-2, memo)
+        memo[n] = result
+    return result
+
+for x in range(1, 1000):
+    memo = [None] * (x + 1)
+    timestart = datetime.now()
+    answer = fib(x, memo)
+    timeend = datetime.now()
+    totaltime = timeend - timestart
+    print str(x) + " " + str(answer) + " " + str (totaltime)
